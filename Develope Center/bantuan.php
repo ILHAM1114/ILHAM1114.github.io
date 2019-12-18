ffed44ac4737a285dcec0c68eabb51267e67e666
oscar-i <55249506+oscar-i@users.noreply.github.com>
2019-12-18 23:57:05 +0700

    as

--- Develope Center/bantuan.php
+++ Develope Center/bantuan.php
@@ -1,6 +1,4 @@
 <?php
 
 $myObj = new \stdClass();
-$myObj->name = 'E-Book adalah layanan baca buku digital yang memberi akses kepada anggota untuk meminjam dan membaca buku dari berbagai genre seperti art,musik,religi,recipes,fantasy,romance,science, biography yang digunakan untuk kebutuhan internal maupun publik yang dapat diakses melalui website E-Book ini.';
-$myJSON = json_encode($myObj->name);
-echo $myJSON;
+$myObj->'E-Book adalah layanan baca buku digital yang memberi akses kepada anggota untuk meminjam dan membaca buku dari berbagai genre seperti art,musik,religi,recipes,fantasy,romance,science, biography yang digunakan untuk kebutuhan internal maupun publik yang dapat diakses melalui website E-Book ini.'
